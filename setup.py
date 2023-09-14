008f200524b22e23488bac7d97bcd111d89a5eff
zabbix-byte <zabbix@ztrunk.space>
2023-09-14 18:38:21 +0200

    preaparing for uploading to pip

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='chromium-pypulse',
+    name='pulse',
     packages=find_packages(),
     version='0.1',
     description='Python, create desktop applications based on Chromium',

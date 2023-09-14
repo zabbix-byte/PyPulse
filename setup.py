29192f00ff66da2d3c740b183ace3c334ecfcc8b
zabbix-byte <zabbix@ztrunk.space>
2023-09-14 18:05:31 +0200

    preaparing for uploading to pip

--- setup.py
+++ setup.py
@@ -4,17 +4,20 @@ setup(
     name='chromium-pypulse',
     packages=find_packages(),
     version='0.1',
-    description='Open-source project that simplifies the creation of desktop applications based on Chromium',
+    description='Python, create desktop applications based on Chromium',
+    long_description='Python, Open-source project that simplifies the creation of desktop applications based on Chromium',
     author='zabbix-byte',
     license='MIT',
-    install_requires = ['cefpython3==66.1', 'Jinja2==3.1.2', 'wxPython==4.2.1'],
+    install_requires=['cefpython3==66.1', 'Jinja2==3.1.2', 'wxPython==4.2.1'],
     classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3.8',
-  ],
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Development Status :: 3 - Alpha',
+        # Define that your audience are developers
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',   # Again, pick a license
+        'Programming Language :: Python :: 3.8',
+    ],
     entry_points={
         'console_scripts': [
             'pypulse-manage = pypulse.manage:main'

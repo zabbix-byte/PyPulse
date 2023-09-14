2c077e075c7c20866063ef5c08473599f74bcec8
zabbix-byte <zabbix@ztrunk.space>
2023-09-14 22:38:42 +0200

    desc

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 from setuptools import find_packages, setup
+import markdown2
+
+
+with open("README.md", "r", encoding="utf-8") as readme_file:
+    long_description = readme_file.read()
+
+# Convert Markdown to HTML
+long_description = markdown2.markdown(long_description)
 
 setup(
     name='python-pulse',
     packages=find_packages(),
     version='0.1',
     description='Python, create desktop applications based on Chromium',
+    long_description=long_description,
+    long_description_content_type="text/html",
     author='zabbix-byte',
     license='MIT',
-    install_requires=['cefpython3==66.1', 'Jinja2==3.1.2', 'wxPython==4.2.1'],
+    install_requires=['cefpython3==66.1', 'Jinja2==3.1.2', 'wxPython==4.2.1', 'markdown2'],
     classifiers=[
         # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Development Status :: 3 - Alpha',

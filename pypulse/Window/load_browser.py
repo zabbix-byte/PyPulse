360e105af9af00525c8423cfb3fb8480cfe08f02
limbocingo <mrcingosup@gmail.com>
2023-12-15 17:59:57 +0100

    change logging system & refactor code

--- pypulse/Window/load_browser.py
+++ pypulse/Window/load_browser.py
@@ -1,32 +1,14 @@
 import threading
 
-from pypulse.Aplication import Vars
 from pypulse.Socket import run_socket
 
-from .browser import Browser, MainFrame
+from .app import App
 
 
-class LoadBrowser():
-    def __init__(self, title: str = 'PyPulse App',
-                 debug: bool = True,
-                 debug_file_name: str = 'debug.log',
-                 window_size_x: int = 1270,
-                 window_size_y: int = 720,
-                 icon_path: str = None,
-                 border_less: bool = False
-                 ) -> None:
-
+class LoadBrowser:
+    def __init__(*args, **kwargs) -> None:
         server_thread = threading.Thread(target=run_socket)
         server_thread.daemon = True
         server_thread.start()
 
-        Browser(title, debug, debug_file_name, window_size_x, window_size_y, icon_path, border_less)
-
-        while (not MainFrame.instance):
-            pass
-
-    @staticmethod
-    def go_to_path(path: str):
-        if MainFrame.instance:
-            MainFrame.instance.GetMainFrame().LoadUrl(
-                f'http://127.0.0.1:{Vars.INTERNAL_HTTP_SERVER_PORT}/{path}')
+        App(**kwargs)

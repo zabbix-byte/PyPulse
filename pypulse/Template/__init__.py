65267ecbb7fffe5d7b0fb2bbaa44a477f016c0eb
mrcingo <mrcingosup@gmail.com>
2025-04-05 14:15:16 +0200

    Add the hability to disable the context menu and render JSON

--- pypulse/Template/__init__.py
+++ pypulse/Template/__init__.py
@@ -1,4 +1,4 @@
-from pypulse.Template.render import RenderTemplate
+from pypulse.Template.render import RenderTemplate, RenderJson
 from pypulse.Template.redirect import Redirect
 from pypulse.Template.reload import Reload
 from pypulse.Template.template import Template
